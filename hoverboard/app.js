34868cb93c4cda01f28d577baa769f824191da83
SantiG912 <santiagoagrandoli03@gmail.com>
2025-10-20 10:22:04 -0300

    Ahora el usuario elige la cantidad de cuadrados

--- hoverboard/app.js
+++ hoverboard/app.js
@@ -1,12 +1,27 @@
-const SQUARES = 396; /*COMO USUARIO ELEGIR LA CANTIDAD DE CUADRADOS*/
+/*COMO USUARIO ELEGIR LA CANTIDAD DE CUADRADOS*/
 const COLORS = ["#156064", "#c33c54", "#72a98f", "#d0cfec", "#5aa9e6"]; //COOLORS!!!
 const $main = document.getElementById("container");
+const $squaresInput = document.getElementById("squaresInput");
+const $generateBtn = document.getElementById("generate");
 const $chkTheme = document.getElementById("theme");
 $chkTheme.addEventListener("change", () => {
     document.body.classList.toggle("light");
 })
 
-function generateSquares(squares = 400){
+const SQUARES = $generateBtn.addEventListener("click", () => {
+    let numSquares = parseInt($squaresInput.value);
+    if (!isNaN(numSquares) && numSquares > 0) {
+        $main.innerHTML = "";
+        generateSquares(numSquares);
+    } else {
+        alert("Por favor, ingrese un número válido de cuadrados.");
+        numSquares = 396;
+        $main.innerHTML = "";
+        generateSquares(numSquares);
+    }
+});
+
+function generateSquares(squares = 396){
     for(let i = 0; i < squares; i++){
         const $square = document.createElement("div");
         $square.classList.add("square");
